a396d66237096d4571c9a8e8805cee312dd28422
Omar ElHagin <omar.elhagin@ibm.com>
2023-11-15 18:33:40 +0200

    feat: add redis config and packages

--- app.module.ts
+++ app.module.ts
@@ -4,9 +4,16 @@ import { AppService } from './app.service';
 import { SearchController } from './src/controllers/search/search.controller';
 import { SearchService } from './src/services/search/search.service';
 import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [CacheModule.register()],
+  imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
+  ],
   controllers: [AppController, SearchController],
   providers: [AppService, SearchService],
 })
