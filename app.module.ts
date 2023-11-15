981dbc8fc07a25727d400271828daeaf41716681
Omar ElHagin <omar.elhagin@ibm.com>
2023-11-15 18:54:07 +0200

    fix: commented out redis as it is giving an error

--- app.module.ts
+++ app.module.ts
@@ -4,15 +4,15 @@ import { AppService } from './app.service';
 import { SearchController } from './src/controllers/search/search.controller';
 import { SearchService } from './src/services/search/search.service';
 import { CacheModule } from '@nestjs/cache-manager';
-import * as redisStore from 'cache-manager-redis-store';
+// import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     CacheModule.register({
       isGlobal: true,
-      store: redisStore,
-      host: 'localhost',
-      port: 6379,
+      // store: redisStore,
+      // host: 'localhost',
+      // port: 6379,
     }),
   ],
   controllers: [AppController, SearchController],
