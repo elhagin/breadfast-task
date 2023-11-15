789589033e76989e6055b929152eff0887b521a4
Omar ElHagin <omar.elhagin@ibm.com>
2023-11-15 18:08:51 +0200

    refactor: add isbn path param inside

--- src/controllers/search/search.controller.ts
+++ src/controllers/search/search.controller.ts
@@ -1,12 +1,10 @@
-import { Controller, Param } from '@nestjs/common';
-import axios from 'axios';
+import { Controller, Get, Param } from '@nestjs/common';
 import { Book } from 'src/interfaces/book.interface';
 
 @Controller('search')
 export class SearchController {
   @Get(':isbn')
-  async findBookNameByISBN(@Param params: any): Promise<Book[]> {
-    const ISBN: string = params.isbn;
+  async findBookNameByISBN(@Param('isbn') ISBN: string): Promise<string> {
     let foundBook: Book = { name: '' };
     
     return foundBook.name;
