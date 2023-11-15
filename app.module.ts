6f3e05bff325609facc7f943e6bee075f19a80f6
Omar ElHagin <omar.elhagin@ibm.com>
2023-11-15 18:50:23 +0200

    fix: add isGlobal to cache import

--- app.module.ts
+++ app.module.ts
@@ -9,6 +9,7 @@ import * as redisStore from 'cache-manager-redis-store';
 @Module({
   imports: [
     CacheModule.register({
+      isGlobal: true,
       store: redisStore,
       host: 'localhost',
       port: 6379,
