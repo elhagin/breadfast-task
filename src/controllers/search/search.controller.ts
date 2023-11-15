df63e2988c22ff5f6435bc54f21da79ddc2d43d8
Omar ElHagin <omar.elhagin@ibm.com>
2023-11-15 18:18:14 +0200

    feat: inject search service

--- src/controllers/search/search.controller.ts
+++ src/controllers/search/search.controller.ts
@@ -1,8 +1,11 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { Book } from 'src/interfaces/book.interface';
+import { SearchService } from 'src/services/search/search.service';
 
 @Controller('search')
 export class SearchController {
+  constructor(private searchService: SearchService) {}
+  
   @Get(':isbn')
   async findBookNameByISBN(@Param('isbn') ISBN: string): Promise<string> {
     let foundBook: Book = { name: '' };
