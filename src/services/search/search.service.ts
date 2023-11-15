c0accdfdaa571fd8ed2aa38734cfcf36faaa5bbc
Omar ElHagin <omar.elhagin@ibm.com>
2023-11-15 18:08:26 +0200

    feat: call API and return bookObj in service

--- src/services/search/search.service.ts
+++ src/services/search/search.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { Book } from 'src/interfaces/book.interface';
 
 @Injectable()
 export class SearchService {
-  
+  async findBookByISBN(ISBN: string): Promise<Book> {
+    const response = await axios.get(
+      `https://openlibrary.org/isbn/${ISBN}.json`,
+    );
+    console.log(response);
+    const bookObj = {
+      name: response.data.title ?? 'Error: Could not retrieve title',
+    };
+    return bookObj;
+  }
 }
