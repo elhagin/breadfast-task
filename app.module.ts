ee22cf91ca1441c1dfad346d3c647d5f7722db05
Omar ElHagin <omar.elhagin@ibm.com>
2023-11-15 18:01:14 +0200

    feat: generate search service

--- app.module.ts
+++ app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SearchController } from './src/controllers/search/search.controller';
+import { SearchService } from './src/services/search/search.service';
 
 @Module({
   imports: [],
   controllers: [AppController, SearchController],
-  providers: [AppService],
+  providers: [AppService, SearchService],
 })
 export class AppModule {}
